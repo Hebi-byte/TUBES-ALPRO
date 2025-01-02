1fa0a309fb1dc4ce96d5048d998a6800d1d9b944
hary06-web <hary24036@gmail.com>
2025-01-03 01:47:49 +0700

    Update edit.java

--- edit.java
+++ edit.java
@@ -6,19 +6,17 @@ public class edit {
         ArrayList<Integer> harga = base.harga;
         ArrayList<String> merk = base.merk;
         Scanner input = new Scanner(System.in);
-        int no = 1;
         System.out.println("==================================");
         System.out.println("Edit data mobil"); 
         System.out.println("==================================");
 
         for (int i = 0; i < harga.size(); i++) {
-                System.out.print(no +" Merk Mobil : " + merk.get(i)+" ||");
+                System.out.print((i+1) +" Merk Mobil : " + merk.get(i)+" ||");
                 System.out.println("Harga Mobil : " + harga.get(i));
-                no++;
         }
         System.out.println("==================================");
         int sellect = input.nextInt();
-            if (sellect <= harga.size()) {
+            if (sellect > 0 &&sellect <= harga.size()) {
                 sellect--;
                 System.out.println("merk mobil : "+merk.get(sellect)+" || harga mobil : "+harga.get(sellect));
                 System.out.println("==================================");
