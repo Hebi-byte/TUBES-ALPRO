abdadfd12d9b21ef3bd426a623865abf3830b626
hary06-web <hary24036@gmail.com>
2025-01-03 01:47:23 +0700

    Create update.java

---
+++ update.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class update {
+    void updatedata(){
+        System.out.println("==================================");
+        System.out.println("Update data mobil");
+        System.out.println("==================================");
+        System.out.println("1. Update merk mobil dan harga mobil");
+        System.out.println("2. hapus data mobil");
+        System.out.println("0. kembali");
+        System.out.println("==================================");
+
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        switch (x) {
+            case 1:
+                edit obj = new edit();
+                obj.editmobil();
+                break;
+            // case 2:
+            //     hapus obj1 = new hapus();
+            //     obj1.hapusmobil();
+            //     break;
+            case 0:
+                return;
+            default:
+                System.out.println("Pilihan tidak ada");
+                updatedata();
+        }
+   } 
+}
