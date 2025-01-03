817eb48e0ba0423451445e09d7a650f7a628e71e
hary06-web <hary24036@gmail.com>
2025-01-03 20:39:20 +0700

    Update nilai.java

--- nilai.java
+++ nilai.java
@@ -1,32 +1,40 @@
 import java.util.ArrayList;
 
 public class nilai {
-    void nilaitertinggi(){
-       ArrayList<Integer> harga = base.harga;
-        ArrayList<String> merk = base.merk;
-        ArrayList<Integer> ppn = base.ppn;
-        
-        int max = harga.get(0);
-        for (int i = 0; i < harga.size(); i++) {
-            if (harga.get(i) > max) {
-                max = harga.get(i);
-            }
-        }
-        System.out.println("Merk Mobil : " + merk.get(harga.indexOf(max)) + " || Harga Mobil : " + max + " || Harga PPN : " + ppn.get(harga.indexOf(max)));
-    } 
-
-    void nilaiterendah(){
+    void nilaitertinggi() {
         ArrayList<Integer> harga = base.harga;
         ArrayList<String> merk = base.merk;
         ArrayList<Integer> ppn = base.ppn;
-        
+
+        int max = harga.get(0);
+        String maxmerk = merk.get(0);
+        int maxppn = ppn.get(0);
+        for (int i = 0; i < harga.size(); i++) {
+            if (harga.get(i) > max) {
+                max = harga.get(i);
+                maxmerk = merk.get(i);
+                maxppn = ppn.get(i);
+            }
+        }
+        System.out.println("Merk Mobil "+maxmerk + " || Harga Mobil : " + max +" || Harga PPN : " + maxppn);
+    } 
+
+    void nilaiterendah() {
+        ArrayList<Integer> harga = base.harga;
+        ArrayList<String> merk = base.merk;
+        ArrayList<Integer> ppn = base.ppn;
+
         int min = harga.get(0);
+        String minmerk = merk.get(0);
+        int minppn = ppn.get(0);
         for (int i = 0; i < harga.size(); i++) {
             if (harga.get(i) < min) {
                 min = harga.get(i);
+                minmerk = merk.get(i);
+                minppn = ppn.get(i);
             }
         }
-        System.out.println("Merk Mobil : " + merk.get(harga.indexOf(min)) + " || Harga Mobil : " + min + " || Harga PPN : " + ppn.get(harga.indexOf(min)));
+        System.out.println("Merk Mobil "+minmerk + " || Harga Mobil : " + min +" || Harga PPN : " + minppn);
 
     }
 }
