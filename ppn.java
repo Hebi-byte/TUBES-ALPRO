c7e794831554da42a337ffbc38e07c5e444fa8b1
Riyan315 <riyangaxys03@gmail.com>
2025-01-03 16:28:13 +0700

    Create ppn.java

---
+++ ppn.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ppn {
+    void ppnmobil() {
+        ArrayList<Integer> harga = base.harga;
+        ArrayList<Integer> ppn = base.ppn;
+
+        for (int i = 0; i < harga.size(); i++) {
+            ppn.add((harga.get(i) * 12 / 100) + harga.get(i))
+        }
+    }
+    
+}
