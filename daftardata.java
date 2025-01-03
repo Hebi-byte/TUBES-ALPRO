429163b41a6cc10dbaa06d3e89fca60310245643
Riyan315 <riyangaxys03@gmail.com>
2025-01-03 16:28:48 +0700

    Update daftardata.java

--- daftardata.java
+++ daftardata.java
@@ -5,6 +5,11 @@ public class daftardata {
     void daftardatamobil() {
         ArrayList<Integer> harga = base.harga;
         ArrayList<String> merk = base.merk;
+        ArrayList<Integer> ppn = base.ppn;
+
+        ppn obj1 = new ppn();
+        obj1.ppnmobil();
+
         Scanner input = new Scanner(System.in);
         System.out.println("==================================");
         System.out.println("Daftar Harga Mobil");
@@ -12,6 +17,7 @@ public class daftardata {
         for (int i = 0; i < harga.size(); i++) {
                 System.out.print((i+1)+". Merk Mobil : " + merk.get(i)+" || ");
                 System.out.println("Harga Mobil : " + harga.get(i));
+                System.out.println("harga PPN :" + ppn.get(i));
         }
         System.out.println("==================================");
         System.out.println("0. kembali");
